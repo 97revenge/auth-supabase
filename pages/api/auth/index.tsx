9ab85924a27ae767b0c7bba0bed1ad7463584636
Matheus Pereira <empregos97@gmail.com>
2024-01-19 15:09:41 -0300

    the Next.js API route for authentication with Supabase. It leverages Supabase for OAuth sign-in, sets up cookie handling for session management, and redirects users to the OAuth provider, ensuring a smooth authentication flow. The provided route configuration details demonstrate efficient code bundling.

--- pages/api/auth/index.tsx
+++ pages/api/auth/index.tsx
@@ -1,17 +1,41 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/lib/supabase/supabase";
 
+import { CookieOptions, createServerClient, serialize } from "@supabase/ssr";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return req.cookies[name];
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          res.appendHeader("Set-Cookie", serialize(name, value, options));
+        },
+        remove(name: string, options: CookieOptions) {
+          res.appendHeader("Set-Cookie", serialize(name, "", options));
+        },
+      },
+    }
+  );
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   const {
     data: { url },
     error,
   } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "http://localhost:3000/api/session",
+      redirectTo: `http://localhost:3000/config/session=${session}`,
     },
   });
 
--- pages/config/index.tsx
+++ pages/config/index.tsx
@@ -5,16 +5,48 @@ import {
 } from "@supabase/auth-helpers-nextjs";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
+import {
+  createServerClient,
+  type CookieOptions,
+  serialize,
+} from "@supabase/ssr";
+
 export default function Page({ data }: { data: any }) {
-  return <div>{data} </div>;
+  return <div>{JSON.stringify(data)} </div>;
 }
 
 export const getServerSideProps: GetServerSideProps = async (
-  ctx: GetServerSidePropsContext
+  context: GetServerSidePropsContext
 ) => {
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return context.req.cookies[name];
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          context.res.appendHeader(
+            "Set-Cookie",
+            serialize(name, value, options)
+          );
+        },
+        remove(name: string, options: CookieOptions) {
+          context.res.appendHeader("Set-Cookie", serialize(name, "", options));
+        },
+      },
+    }
+  );
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
   return {
     props: {
-      data: "ok",
+      user: session?.user,
     },
   };
 };
