9fd9375ca4ce38214b2deb5e37e71228d444a0ab
Matheus Pereira <empregos97@gmail.com>
2024-01-22 16:32:45 -0300

    prisma issue commit

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
-export default function middleware(req: NextRequest) {
+export default async function middleware(req: NextRequest) {
   const oauth = req.nextUrl.searchParams.get("oauth");
+
+  const prisma = new PrismaClient();
+
+  const instance = await prisma.user.findUnique({
+    where: {
+      clientGoogle: oauth as string,
+    },
+  });
 
   if (oauth) {
     return NextResponse.next();
